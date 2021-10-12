fc845e82c7b50eb3ab042d8fab37a656a80335ff
pdrontzas <philipos.d@esl-asia.com>
2021-10-12 21:25:17 +0200

    Added docker - still needs work

--- client/src/views/dashboard/index.jsx
+++ client/src/views/dashboard/index.jsx
@@ -1,23 +1,23 @@
 // import logo from './logo.svg';
-import './index.scss';
-import '../../services/http-service';
-import { fetchStudents } from '../../services/http-service';
-import { useEffect, useState } from 'react';
+import "./index.scss";
+import "../../services/http-service";
+import { fetchStudents } from "../../services/http-service";
+import { useEffect, useState } from "react";
 
 function Dashboard() {
   const [data, setData] = useState({});
 
   useEffect(() => {
-    console.log('test from Main');
-    fetchStudents('param123').then(res => {
-      console.log(res)
+    console.log("test from Main");
+    fetchStudents("param123").then((res) => {
+      console.log(res);
       setData(res);
     });
-  },[]);
+  }, []);
 
   return (
     <div id="dashboard">
-      <p>Dashboard</p>
+      <p>Dashboarddd</p>
     </div>
   );
 }
